2e0ceaf81528c669c8dbc4e67bdfbcbf4855f9a7
mellis <mellis.eriecon@gmail.com>
2021-08-04 11:43:32 -0400

    in lesson 2 / Store the Employees Names

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -5,8 +5,36 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
+        static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                //
+                // Create a new Employee instance
+                Employee currentEmployee = new Employee();
+                employees.Add(input);
+            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
         static void Main(string[] args)
         {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
             // string greeting = "hello";
             // greeting = greeting + "world";
             // Console.WriteLine("greeting" + greeting);
@@ -72,17 +100,71 @@ namespace CatWorx.BadgeMaker
             // string thirdFood = favFoods[2];
             // Console.WriteLine("I like {0}, {1},and {2}", firstFood, secondFood, thirdFood);
             // Changes made for git test
-            List<string> employees = new List<string>() { "adam", "amy" };
+            // List<string> employees = new List<string>();
+            // // { "adam", "amy" };
+            // // Collect user values until the value is an empty string
+            // while (true)
+            // {
+            //     Console.WriteLine("Please enter a name: ");
+            //     string input = Console.ReadLine();
+            //     employees.Add(input);
+            // }
+            // for (int i = 0; i < employees.Count; i++)
+            // {
+            //     Console.WriteLine(employees[i]);
+            // }
 
-            employees.Add("barbara");
-            employees.Add("billy");
+            // employees.Add("barbara");
+            // employees.Add("billy");
+
+            // Get a name from the console and assign it to a variable
+
 
             // Console.WriteLine("My employees include {0}, {1}, {2}, {3}", employees[0], employees[1], employees[2], employees[3]);
-            for (int i = 0; i < employees.Count; i++)
-            {
-                Console.WriteLine("My employees include " + employees[i]);
-            }
+            // for (int i = 0; i < employees.Count; i++)
+            // {
+            //     Console.WriteLine("My employees include " + employees[i]);
+            // }
+            //     List<string> employees = new List<string>();
+
+            //     // Collect user values until the value is an empty string
+
+            //     while (true)
+
+            //     {
+
+            //         Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+            //         string input = Console.ReadLine();
+
+            //         employees.Add(input);
+
+            //     }
+
+            //     for (int i = 0; i < employees.Count; i++)
+
+            //     {
+
+            //         Console.WriteLine(employees[i]);
+
+            //     }
+            // }
+
+            // while (true)
+            // {
+            //     Console.WriteLine("Please enter a name: (leave empty to exit): ");
+            //     string input = Console.ReadLine();
+            //     // Break if the user hits ENTER without typing a name
+            //     if (input == "")
+            //     {
+            //         break;
+            //     }
+            //     employees.Add(input);
+            // }
+
+            // This is our employee-getting code now
 
         }
+
     }
 }
